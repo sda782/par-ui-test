c98a00aa55c5bd4b1cffa863a31c2735868f2907
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2021-10-27 12:31:41 +0200

    uitest

---
+++ uitest/uitest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace uitest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static readonly string driver_path = "../";
+        private static IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
