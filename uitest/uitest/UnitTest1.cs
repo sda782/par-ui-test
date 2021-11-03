37619b5779615128156f583f5488474eb7c20669
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2021-11-03 11:09:39 +0100

    add record ui test + fixes

--- uitest/uitest/UnitTest1.cs
+++ uitest/uitest/UnitTest1.cs
@@ -12,12 +12,13 @@ namespace uitest
     {
         private static readonly string driver_path = @"C:\Users\Andreas\Documents\GitHub\Programming\_Webdrivers";
         private static IWebDriver driver;
-
+        private static WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         [ClassInitialize]
         public static void init_setup(TestContext testContext)
         {
             //driver = new ChromeDriver(driver_path);
             driver = new FirefoxDriver(driver_path);
+            
         }
 
         [TestMethod]
@@ -26,7 +27,7 @@ namespace uitest
             string url = "https://sda782.github.io/par-web";
             driver.Navigate().GoToUrl(url);
             Assert.AreEqual(driver.Title, "Document");
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // decorator pattern?
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // decorator pattern?
             IWebElement record_table = wait.Until(d => d.FindElement(By.Id("record_table")));
             Assert.IsTrue(record_table.Text.Contains("shit label"));
             Console.WriteLine(record_table.Text);
@@ -35,12 +36,29 @@ namespace uitest
             IWebElement search = driver.FindElement(By.Id("searchinput")); //input field
             search.SendKeys("record 2"); //search word
             IWebElement searchbutton = driver.FindElement(By.Id("searchbutton")); //button
-
-            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            record_table = wait2.Until(d => d.FindElement(By.Id("record_table")));
+            searchbutton.Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // decorator pattern?
+            record_table = wait.Until(d => d.FindElement(By.Id("record_table")));
             Console.WriteLine(record_table.Text);
             Assert.IsTrue(record_table.Text.Contains("record 2"));
             Assert.IsFalse(record_table.Text.Contains("record 1"));
+
+            //add record
+            IWebElement addname = driver.FindElement(By.Id("name_input")); //input field
+            search.SendKeys("gigarecord"); //search word
+            IWebElement addlabel = driver.FindElement(By.Id("label_input")); //input field
+            search.SendKeys("notgoodlabel"); //search word
+            IWebElement addruntime = driver.FindElement(By.Id("run_time_input")); //input field
+            search.SendKeys("160"); //search word
+            IWebElement adddate = driver.FindElement(By.Id("date_input")); //input field
+            search.SendKeys(DateTime.Now.ToString()); //search word
+            IWebElement addtrack = driver.FindElement(By.Id("track_input")); //input field
+            search.SendKeys("20"); //search word
+            IWebElement addbutton = driver.FindElement(By.Id("add_button")); //button
+            addbutton.Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // decorator pattern?
+            record_table = wait.Until(d => d.FindElement(By.Id("record_table")));
+            Assert.IsTrue(record_table.Text.Contains("gigarecord"));
         }
         /*   [TestMethod]
            public void TestMethod2()
