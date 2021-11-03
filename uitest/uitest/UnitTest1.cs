1b9eec7daffcac5008335a053c636d466241eac6
Andreas <saerdna.petersen@gmail.com>
2021-11-03 11:35:29 +0100

    add ui test

--- uitest/uitest/UnitTest1.cs
+++ uitest/uitest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -12,13 +13,13 @@ namespace uitest
     {
         private static readonly string driver_path = @"C:\Users\Andreas\Documents\GitHub\Programming\_Webdrivers";
         private static IWebDriver driver;
-        private static WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        private static WebDriverWait wait;
         [ClassInitialize]
         public static void init_setup(TestContext testContext)
         {
             //driver = new ChromeDriver(driver_path);
             driver = new FirefoxDriver(driver_path);
-            
+
         }
 
         [TestMethod]
@@ -44,34 +45,27 @@ namespace uitest
             Assert.IsFalse(record_table.Text.Contains("record 1"));
 
             //add record
+            string record_name = "testrecord2";
             IWebElement addname = driver.FindElement(By.Id("name_input")); //input field
-            search.SendKeys("gigarecord"); //search word
+            addname.SendKeys(record_name); //search word
             IWebElement addlabel = driver.FindElement(By.Id("label_input")); //input field
-            search.SendKeys("notgoodlabel"); //search word
+            addlabel.SendKeys("notgoodlabel"); //search word
             IWebElement addruntime = driver.FindElement(By.Id("run_time_input")); //input field
-            search.SendKeys("160"); //search word
+            addruntime.SendKeys("160"); //search word
             IWebElement adddate = driver.FindElement(By.Id("date_input")); //input field
-            search.SendKeys(DateTime.Now.ToString()); //search word
+            adddate.SendKeys("12122021");
+            adddate.SendKeys(Keys.Tab);
+            adddate.SendKeys("1625");
+            //search word
             IWebElement addtrack = driver.FindElement(By.Id("track_input")); //input field
-            search.SendKeys("20"); //search word
+            addtrack.SendKeys("20"); //search word
             IWebElement addbutton = driver.FindElement(By.Id("add_button")); //button
             addbutton.Click();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // decorator pattern?
             record_table = wait.Until(d => d.FindElement(By.Id("record_table")));
-            Assert.IsTrue(record_table.Text.Contains("gigarecord"));
+            Assert.IsTrue(record_table.Text.Contains(record_name));
         }
-        /*   [TestMethod]
-           public void TestMethod2()
-           {
-               string filter_name = "record 1";
-               string url = "https://sda782.github.io/par-web?name=" + filter_name;
-               driver.Navigate().GoToUrl(url);
-               Assert.AreEqual(driver.Title, "Document");
-               WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // decorator pattern?
-               IWebElement record_table = wait.Until(d => d.FindElement(By.Id("record_table")));
-               Assert.IsTrue(record_table.Text.Contains(filter_name));
 
-           }*/
         [ClassCleanup]
         public static void clean_up()
         {
